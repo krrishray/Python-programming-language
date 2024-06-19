7cefe147b1784b17e2c21a74a3608bba3f28d8f4
krrish ray <krrishray236@gmail.com>
2024-06-20 01:46:11 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,13 @@
+print("hello world") #simpale print 
+
+human = "krrish"
+print("hi my name is",human)
+
+humans = input("enter your genretion: ") #taking input
+print("my genretion name  is ",humans)
+
+age = float(input("enter your age:"))
+print("you can lern python too ",age)
+# printing 0 to 100 in a range  
+for i in range(0,101,2): 
+    print(i)
